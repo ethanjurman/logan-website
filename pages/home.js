4153710b42b31de891c9a24a9d909eecf0a04eb5
Ethan H. Jurman <ehj2229@rit.edu>
2018-02-11 16:55:37 -0500

    tram-one files

---
+++ pages/home.js
@@ -0,0 +1,16 @@
+const Tram = require('tram-one')
+const html = Tram.html({
+  header: require('../elements/header')
+})
+
+module.exports = () => {
+  return html`
+    <div>
+      <header></header>
+      <div>
+        Thank you for using Tram-One!<br>
+        To get started, edit <code>tram-one-app/pages/home.js</code>.
+      </div>
+    </div>
+  `
+}
