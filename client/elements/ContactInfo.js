66ae8b04426c60727932cf397d3e9a326ae4417b
jjurman <jjurman@constantcontact.com>
2018-04-22 19:22:01 -0400

    real email

--- client/elements/ContactInfo.js
+++ client/elements/ContactInfo.js
@@ -12,8 +12,8 @@ module.exports = () => {
   return html`
     <div style=${contactStyle}>
       <h4> How to reach me: </h4>
-      <IconLink icon="fas fa-envelope" link="mailto:joneslogan46@yahoo.com">
-        joneslogan46@yahoo.com
+      <IconLink icon="fas fa-envelope" link="mailto:logan@eiferersarmory.com">
+        logan@eiferersarmory.com
       </IconLink>
       <IconLink icon="fab fa-facebook" link="https://www.facebook.com/VerbotenEiferer/">
         The Eiferer's Armory on Facebook
