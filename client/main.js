d98ba62e899c491f86a67eeb92a79471326a39c1
Ethan Jurman <ethanjurman@gmail.com>
2018-04-22 15:10:58 -0400

    about page

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ const route = Tram.route()
 
 const app = new Tram()
 app.addRoute('/', require('./pages/Chrome'), [
-  route('/about', require('./pages/HomePage')),
   route('/images/:albumPage', require('./pages/ImageSetPage')),
   route('/images/:albumPage/:imageId', require('./pages/ImageFullScreen')),
   route('/404', require('./pages/404'))
--- client/pages/Chrome.js
+++ client/pages/Chrome.js
@@ -7,11 +7,50 @@ const pageStyle = `
   margin: 2em;
 `
 
+const hrStyle = `
+  box-shadow: 0px 0px 1px black;
+`
+
+const contactStyle = `
+  font-size: 2em;
+`
+
+const linkStyle = `
+  text-decoration: none;
+`
+
+const renderLink = ({icon, text, link}) => {
+  return html`
+    <div>
+      <a href=${link} style=${linkStyle}>
+        <i class=${icon} ></i>
+        ${text}
+      </a>
+    </div>
+  `
+}
+
 module.exports = (store, actions, params, subroute) => {
   return html`
     <div style=${pageStyle}>
       <Header />
       ${subroute}
+      <hr style=${hrStyle} />
+      <div style=${contactStyle}>
+        <h4> How to reach me: </h4>
+        ${renderLink({
+          icon: "fas fa-envelope",
+          text: " Contact the email",
+          link: "mailto:logan@eiferersarmory.com"
+        })}
+        ${renderLink({
+          icon: "fab fa-facebook",
+          text: " Check out the facebook page",
+          link: "https://www.facebook.com/VerbotenEiferer/"
+        })}
+      </div>
+      <hr style=${hrStyle} />
+
     </div>
   `
 }
